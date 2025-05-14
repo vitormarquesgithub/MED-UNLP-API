7c26b2dac6f450de3e14373890c18e09648d2efc
Vitor Marques <vitormarquesdev@gmail.com>
2025-05-14 16:10:16 -0300

    feat(docker): add Dockerfile and docker-compose configuration for local dev

---
+++ src/main/java/edu/unlp/medflash/med_unp_backend/MedUnlpApiApplication.java
@@ -0,0 +1,13 @@
+package edu.unlp.medflash.med_unp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedUnlpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedUnlpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/unlp/medflash/med_unp_backend/MedUnlpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unlp.medflash.med_unp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedUnlpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
