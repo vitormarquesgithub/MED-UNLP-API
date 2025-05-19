000dfa6b5cef62823d9e6a335512eaef04802e3d
Vitor Marques <vitormarquesdev@gmail.com>
2025-05-19 17:55:39 -0300

    feat(model): extend User with AbstractEntity using UUID

---
+++ src/main/java/edu/unlp/medflash/med_unp_backend/Enum/Role.java
@@ -0,0 +1,6 @@
+package edu.unlp.medflash.med_unp_backend.Enum;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
--- src/main/java/edu/unlp/medflash/med_unp_backend/model/User.java
+++ src/main/java/edu/unlp/medflash/med_unp_backend/model/User.java
@@ -1,28 +1,32 @@
 package edu.unlp.medflash.med_unp_backend.model;
 
+import edu.unlp.medflash.med_unp_backend.Enum.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Table(name = "users")
-@Schema(description = "Entidade que representa um usuário no sistema")
-public class User {
-    @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Schema(description = "ID único do usuário", example = "1")
-    private Long id;
+@Schema(description = "Entity that represents a user in the system")
+public class User extends AbstractEntity {
     
-    @Schema(description = "Email do usuário", example = "user@example.com")
+    @Schema(description = "User email", example = "user@example.com")
+    @Column(unique = true, nullable = false)
     private String email;
     
-    @Schema(description = "Senha criptografada", example = "$2a$10$N9qo8uLOickgx2ZMRZoMy...")
+    @Schema(description = "Encrypted password", example = "$2a$10$N9qo8uLOickgx2ZMRZoMy")
+    @Column(nullable = false)
     private String password;
     
-    @Schema(description = "Papel do usuário", example = "ADMIN")
-    private String role;
+    @Schema(description = "User Role", example = "ADMIN")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
     
-    @Schema(description = "Nome completo do usuário", example = "João da Silva")
+    @Schema(description = "User's full name", example = "Vitor Marques")
+    @Column(nullable = false)
     private String name;
 }
